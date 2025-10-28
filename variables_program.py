d0c94f2d3efc24e4392b1befaf663832359a7152
Debjit Haldar <haldardebjit2003@gmail.com>
2025-10-28 10:21:09 +0530

    Add files via upload

---
+++ variables_program.py
@@ -0,0 +1,16 @@
+
+# 1. VARIABLES
+
+pi = 22 / 7
+print("Value of pi:", pi)
+print("Data type of pi:", type(pi))
+
+# for = 4  # SyntaxError: 'for' is a reserved keyword
+for_value = 4
+print("Value of for_value:", for_value)
+
+P = 10000
+R = 5
+T = 3
+SI = (P * R * T) / 100
+print("Simple Interest for 3 years:", SI)
